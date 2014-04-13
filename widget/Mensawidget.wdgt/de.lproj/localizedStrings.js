ffeab588744c625ea322a8f72d83a0056ca00891
Wolf-Bastian Pöttner <bastian@poettner.de>
2014-04-13 21:09:48 +0200

    Added Mensa widget

---
+++ widget/Mensawidget.wdgt/de.lproj/localizedStrings.js
@@ -0,0 +1,22 @@
+
+var localizedStrings = new Object;
+
+localizedStrings["Mein RSS-Feed"]  = 'Mensa Braunschweig';
+localizedStrings["Neue URL: "]  = "Neue URL: ";
+localizedStrings["Festlegen"]  = "Festlegen";
+localizedStrings["Feed-URLs: "]  = "Feed-URLs: ";
+localizedStrings["Artikellänge"]  = "Artikellänge";
+localizedStrings["Zurücksetzen"]  = "Zurücksetzen";
+localizedStrings["Fertig"]  = "Fertig";
+localizedStrings["Feed ungültig"]  = "Feed ungültig";
+localizedStrings["%s scheint kein gültiger RSS- oder Atom-Feed zu sein."]  = "%s scheint kein gültiger RSS- oder Atom-Feed zu sein.";
+localizedStrings["Keine Objekte gefunden"]  = "Keine Objekte gefunden";
+localizedStrings["Der Feed enthält keine Einträge."]  = "Der Feed enthält keine Einträge.";
+localizedStrings["Keine passenden Objekte zu den Suchbegriffen gefunden."]  = "Keine passenden Objekte zu den Suchbegriffen gefunden.";
+localizedStrings["Keine Objekte zum Anzeigen"]  = "Keine Objekte zum Anzeigen";
+localizedStrings["Der Feed enthält keine Einträge zu den angegebenen Kriterien."]  = "Der Feed enthält keine Einträge zu den angegebenen Kriterien.";
+localizedStrings["Laden"]  = "Laden";
+localizedStrings["%s neu"]  = "%s neu";
+localizedStrings["Kein Feed angegeben"]  = "Kein Feed angegeben";
+localizedStrings["Bitte geben Sie eine gültige Feed-URL ein."]  = "Bitte geben Sie eine gültige Feed-URL ein.";
+localizedStrings['Text'] = 'Essen am ...';
---
+++ widget/README.md
@@ -0,0 +1,5 @@
+MensaFetcherBS Widget
+=====================
+
+This widget can be used on Mac OS X to display the
+Speiseplan of the Mensa in Braunschweig.
